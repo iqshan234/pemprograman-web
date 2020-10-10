c65dc6c2ebf65f9194f68e88e146eb3d8ddad4ed
Mochammad Iqshan Augustino <71367297+iqshan234@users.noreply.github.com>
2020-10-09 17:53:53 -0700

    Add files via upload

---
+++ tugas 4/video/login.php
@@ -0,0 +1,24 @@
+<?php
+
+$user = 'hilman';
+$password = '123';
+if ( isset($_POST['submit']) ) {
+
+if ( $_POST['nama'] == $user &&
+	 $_POST['password' == $password]) {
+
+	header('Location: profile.php?nama' . $user);
+
+}else{
+	echo 'login gagal!';
+}
+}
+
+  
+  ?>
+
+  <form action="profile.php" method="post">
+  		<input type="text" name="nama">
+  		<input type="password" name="password">
+  		<input type="submit" name="submit">
+  </form>
